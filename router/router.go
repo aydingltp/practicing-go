a2a383e89b6e9e6b6acc4251b36a675cfd611ac6
aydingltp <aydngltp@gmail.com>
2021-06-26 21:54:54 +0300

    router

---
+++ router/router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"practicing-go/controllers"
+)
+
+func Hello(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{"status": "success", "message": "Hello i'm ok!", "data": nil})
+}
+
+func SetupRoutes(app *fiber.App) {
+	api := app.Group("/api", logger.New())
+	api.Get("/", Hello)
+
+	user := api.Group("/user")
+	user.Get("/", controllers.GetAllUsers)
+	user.Get("/:id", controllers.GetUser)
+	user.Post("/", controllers.CreateUser)
+	user.Delete("/:id", controllers.DeleteUser)
+}
